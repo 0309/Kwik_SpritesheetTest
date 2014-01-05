0ab8a5639e7ed69b1a02b2c504eb4e7883171616
N.TANAKA <noirpur@gmail.com>
2014-01-05 21:42:32 +0900

    first commit

---
+++ spritesheet.lua
@@ -0,0 +1,11 @@
+-- created with TexturePacker (http://www.texturepacker.com)
+           frames = {
+             
+               { x=230, y=230, width=226, height=226 }, -- masu_1
+               { x=230, y=2, width=226, height=226 }, -- masu_2
+               { x=2, y=230, width=226, height=226 }, -- masu_3
+               { x=2, y=2, width=226, height=226 }, -- masu_4
+           },
+    
+           sheetContentWidth = 512,
+           sheetContentHeight = 512
